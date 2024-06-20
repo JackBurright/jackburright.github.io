705a6f6653aeeb14cd2c8283923a2cf41bcde324
JackBurright <Jrb94233@creighton.edu>
2024-06-20 12:01:08 -0500

    put updated resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/JackBurrightResume.pdf'
+import CV from '../../assets/ResumeJackBurright.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
